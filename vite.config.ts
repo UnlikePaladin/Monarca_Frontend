3189a7419061764dc3f82ee617f796b8629b6eaf
santiagoarista <santiagoaristav@outlook.com>
2026-02-19 20:23:46 -0600

    Add CODEOWNERS file and remove HTTPS configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import fs from 'fs';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/frontend-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/frontend.pem')),
-    },
+    port: 5173,
   }
 });
